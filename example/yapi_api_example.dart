4ecdf9c4b99ac11bd5d3e9a78ee9636993f87a34
zmtzawqlp <zmtzawqlp@live.com>
2025-08-23 14:48:14 +0800

    update example

--- example/yapi_api_example.dart
+++ example/yapi_api_example.dart
@@ -1,23 +1,36 @@
 import 'package:yapi_api/yapi_api.dart';
 
-void main() {
+void main() async {
   final YapiApiHelper apiHelper = YapiApiHelper();
   apiHelper.baseUrl = 'http://yapi.example.com';
-  apiHelper.cookie = 'your_cookie_here';
+  // or set cookie directly if you have already logged in
+  // apiHelper.cookie = 'your_cookie_here';
+  YapiLoginResponse? loginResponse =
+      await apiHelper.login(email: '', password: '');
+  if (loginResponse != null && loginResponse.errcode == 0) {
+    print('login success');
+  } else {
+    print('login failed : ${loginResponse?.errmsg}');
+    return;
+  }
 
-  apiHelper.getInterface(1).then((YapiInterfaceResponse? response) {
-    if (response != null) {
-      print('interface: ${response.data?.title}');
-    } else {
-      print('get interface failed ');
-    }
-  });
+  YapiProjectResponse? projectResponse = await apiHelper.getProject(1);
+  if (projectResponse != null) {
+    print('project : ${projectResponse.data?.name}');
+  } else {
+    print('get project failed ');
+  }
+  YapiInterfaceResponse? interfaceResponse = await apiHelper.getInterface(1);
+  if (interfaceResponse != null) {
+    print('interface : ${interfaceResponse.data?.title}');
+  } else {
+    print('get interface failed ');
+  }
 
-  apiHelper.getProject(1).then((YapiProjectResponse? response) {
-    if (response != null) {
-      print('project : ${response.data?.name}');
-    } else {
-      print('get project failed ');
-    }
-  });
+  var logoutResponse = await apiHelper.logout();
+  if (logoutResponse != null && logoutResponse.errcode == 0) {
+    print('logout success');
+  } else {
+    print('logout failed : ${logoutResponse?.errmsg}');
+  }
 }
